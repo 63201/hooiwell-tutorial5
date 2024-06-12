64fc560b42db6339d0c62494538f532f8074257f
63201 <hooiwelL@gmail.com>
2024-06-13 00:10:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.myapplication
+
+import android.graphics.drawable.AnimationDrawable
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageView
+import com.example.myapplication.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        // Get references to ImageView elements in the layout
+//        val backgroundImageView = findViewById<ImageView>(R.id.backgroundImageView)
+        val animationImageView = findViewById<ImageView>(R.id.animationImageView)
+
+        // Start the background animation
+//        val backgroundAnimationDrawable = backgroundImageView.background as AnimationDrawable
+//        backgroundAnimationDrawable.start()
+
+        // Start the animation for the other images after a delay
+        animationImageView.postDelayed({
+            animationImageView.visibility = ImageView.VISIBLE // Make the animation ImageView
+            animationImageView.setBackgroundResource(R.drawable.animation_list)
+            val animationDrawable = animationImageView.background as AnimationDrawable
+            animationDrawable.start() // Start the animation for the other images
+        }, 1000) // Delay for 1000 milliseconds(1s)
+    }
+
+}
